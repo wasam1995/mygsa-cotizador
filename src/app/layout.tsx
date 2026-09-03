db2924dd0cdbcd9c97dde39fc4e034883f127ed9
wasam1995 <wmoralesluna@outlook.com>
2026-09-02 23:07:01 -0600

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export const viewport: Viewport = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <body className="min-h-screen antialiased">{children}</body>
+      <body className="min-h-screen font-sans antialiased">{children}</body>
     </html>
   );
 }
